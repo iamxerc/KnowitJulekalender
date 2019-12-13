8e17c2bcd72104e12498feab2f1027358a59821d
iamxerc <iamxerc@gmail.com>
2019-12-14 00:57:16 +0100

    Forbedret runtime bugfix

--- 2019/13. des/draw_map.php
+++ 2019/13. des/draw_map.php
@@ -5,7 +5,7 @@ class drawMap {
     function __construct($robot) {
         $this->robot = $robot;
         $this->start = microtime(true);
-        $this->html = "<table style='border-collapse:collapse;'>";
+        $this->html = "<table style='border-collapse:collapse;'>\n";
         if(file_exists($this->robot.".log"))
             $this->log = unserialize(file_get_contents($this->robot.".log"));
         
@@ -13,12 +13,12 @@ class drawMap {
     function isVisited($pos) {
         if(!property_exists($this, 'log')) 
             return;
-        list($y,$x) = $pos;
-        if(array_key_exists(implode(',',$pos), $this->log));
+        
+        if(array_key_exists(implode(',',$pos), $this->log))
             $this->html .= "background-color:green;";
     }
     function drawCell($cell) {
-        $this->html .= "<td style='width:3px;height:3px;";
+        $this->html .= "\t\t<td style='width:3px;height:3px;";
         $this->isVisited([$cell['y'],$cell['x']]);
         if($cell['bottom'] == true)
             $this->html .= "border-bottom:thin solid black;";
@@ -28,21 +28,21 @@ class drawMap {
             $this->html .= "border-top:thin solid black;";
         if($cell['left'] == true) 
             $this->html .= "border-left:thin solid black;";
-        $this->html .= "'></td>";
+        $this->html .= "'></td>\n";
     }
     function draw() {
         global $map;
         foreach($map as $y=>$arrX) {
-            $this->html .= "<tr>";
+            $this->html .= "\t<tr>\n";
             foreach($arrX as $x=>$obj) {
                 list($cx, $cy, $top, $left, $bottom, $right) = array_pad((array) $obj,12,null);
                 $this->drawCell((array) $obj);
             }
-            $this->html .= "</tr>";
+            $this->html .= "\t</tr>\n";
         }
     }
     function __destruct() {
-        $this->html .= "</table>";
+        $this->html .= "</table>\n";
         file_put_contents($this->robot.".html", $this->html);
         $this->stop = microtime(true);
         echo "Runtime ".$this->robot.": ".($this->stop - $this->start)." seconds.\n";
